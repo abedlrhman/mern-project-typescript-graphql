22dfdbdfc30f9e8a48eaeed67255a8013c1082f1
= <=>
2023-02-13 22:18:58 +0300

    add post resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
@@ -5,6 +6,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/Post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -14,12 +16,15 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
-  
+  // without this, apollo will throw an error.
+  await apolloServer.start();
+
   app.listen(4000, () => {
     apolloServer.applyMiddleware({ app });
     console.log("server started on localhost:4000");
